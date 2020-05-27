de3db04cb3b27bbb4b3aec6e0105c30c32757c5c
Egor Chemokhonenko <egor.chemohonenko@ahrefs.com>
2020-05-27 15:27:39 +0300

    init

---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { FormattedMessage, Link, useIntl } from "gatsby-plugin-intl"
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => {
+  const intl = useIntl()
+  return (
+    <Layout>
+      <SEO
+        lang={intl.locale}
+        title={intl.formatMessage({ id: "title" })}
+        keywords={[`gatsby`, `application`, `react`]}
+      />
+      <h1>
+        <FormattedMessage id="hello" />
+      </h1>
+      <p>
+        <FormattedMessage id="textWithLink" values={{
+          link1: msg => (
+            <a href="https://github.com/wiziple/gatsby-plugin-intl" >
+              {msg}
+            </a>
+         )}}  />
+      </p>
+      <p>
+        <FormattedMessage id="welcome" />
+      </p>
+      <p>
+        <FormattedMessage id="build" />
+      </p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">
+        <FormattedMessage id="go_page2" />
+      </Link>
+    </Layout>
+  )
+}
+
+export default IndexPage
